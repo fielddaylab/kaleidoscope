7efa1a438a28268d8e05a4ba437451e5d000783c
Michael Tolly <miketolly@gmail.com>
2016-11-18 15:58:42 -0600

    Working content browser X/arrows buttons

--- user/themes/kaleidoscope/js/expand-content.js
+++ user/themes/kaleidoscope/js/expand-content.js
@@ -1,13 +1,35 @@
 $(function(){
+  var opened_index = null;
+  function open_item(grid, i) {
+    grid = $(grid);
+    var rows = grid.find('.content-details');
+    var items = grid.find('.content-details-item');
+    if (i < 0 || items.length <= i) return;
+    rows.hide();
+    items.hide();
+    var item = $(items[i]);
+    var row = item.parents('.content-details');
+    row.show();
+    item.show();
+    $('html,body').animate({
+      scrollTop: row.offset().top - 150
+    }, 500);
+    opened_index = i;
+  }
   $('.content-entry').click(function(e){
     var entry = $(e.delegateTarget);
-    var i = entry.index();
-    entry.parents('.content-grid').children('.content-details').hide();
-    var details = entry.parents('.content-row').next();
-    details.show();
-    var items = details.children('.content-details-item');
-    items.each(function(j, item){
-      i === j ? $(item).show() : $(item).hide();
-    });
+    var grid = entry.parents('.content-grid');
+    open_item(grid, grid.find('.content-entry').index(entry));
+  });
+  $('.content-details-controls-x').click(function(e){
+    $(e.delegateTarget).parents('.content-details').hide();
+  });
+  $('.content-details-controls-prev').click(function(e){
+    var grid = $(e.delegateTarget).parents('.content-grid');
+    open_item(grid, opened_index - 1);
+  });
+  $('.content-details-controls-next').click(function(e){
+    var grid = $(e.delegateTarget).parents('.content-grid');
+    open_item(grid, opened_index + 1);
   });
 });
